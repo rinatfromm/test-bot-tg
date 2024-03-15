ddda8f5e616dd7c8c2bf047a96139df6558e09cb
Rinat Kov <frommrinat@gmail.com>
2024-03-15 23:54:31 +0100

    correction

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ function App() {
     <div className="App">
       <Header />
       <Routes>
-        <Route index element={<Form />} />
-        <Route path={"/card"} element={<UserCard />} />
+        <Route index element={<UserCard />} />
+        <Route path={"form"} element={<Form />} />
       </Routes>
     </div>
   );
