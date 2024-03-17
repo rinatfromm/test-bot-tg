6b2d300fce3a44d0b8d7ca4c40144c3fb4118e96
Rinat Kov <frommrinat@gmail.com>
2024-03-17 12:10:49 +0100

    correction

--- src/components/userCard/UserCard.jsx
+++ src/components/userCard/UserCard.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { selectUserPhoto } from '../../store/slices/formSlice';
-import styles from './UserCard.module.css';
+import { selectUserPhoto } from './store/slices/formSlice';
 
 const UserCard = () => {
-  const userPhoto = useSelector(selectUserPhoto);
+    const userPhoto = useSelector(selectUserPhoto);
 
-  return (
-    <div className={styles.container}>
-      <h2>User Card</h2>
-      <div className={styles.photoContainer}>
-        {userPhoto && <img src={userPhoto} alt="User" className={styles.photo} />}
-      </div>
-      {/* Добавьте другие детали пользователя, если нужно */}
-    </div>
-  );
+    return (
+        <div>
+            {userPhoto && <img src={userPhoto} alt="User" />}
+        </div>
+    );
 }
 
 export default UserCard;
